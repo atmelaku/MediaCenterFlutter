f81280528ec1b13d4b48c2e61e29dc89da8051c2
Charles Max Crandall <robloxatme.com@gmail.com>
2021-04-20 09:04:16 -0400

    Added new files

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Baby Names",
+      title: "Media Center App",
       home: HomePage(),
     );
   }
