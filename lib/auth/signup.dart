15ef52a136d0fb399829b8c4b852689dfee47968
Charles Max Crandall <robloxatme.com@gmail.com>
2021-04-20 09:20:03 -0400

    Added navigation from homepage to signup and login

---
+++ lib/auth/signup.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() {
+    return _SignUpPageState();
+  }
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign Up')),
+      body: Container(),
+    );
+  }
+}
--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mediacenterflutter/auth/login.dart';
+import 'package:mediacenterflutter/auth/signup.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,7 +14,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Media Center')),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                }, child: Container(child: Text("Login")) ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                }, child: Container(child: Text("Sign Up")) ),
+          ],
+        )
+      ),
     );
   }
 }
