33e992501f2d23e18a1ff441a1cdb2c7492408f1
Melaku <>
2021-04-27 08:53:21 -0400

    create a welcom page, add a log out button so the user can log out

--- lib/homepage/signedin.dart
+++ lib/homepage/signedin.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mediacenterflutter/auth/auth.dart';
+import 'package:mediacenterflutter/models/user_list.dart';
+import 'homepage.dart';
 
 class signedInPage extends StatefulWidget {
   @override
@@ -8,14 +11,41 @@ class signedInPage extends StatefulWidget {
 }
 
 class _signedInState extends State<signedInPage> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome Back')),
-      body: Container(
-          child: Column(
-          )
+      appBar: AppBar(title: Text('Welcome Back'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person,),
+            label: Container(
+              child: ElevatedButton(
+                child: Text(
+                    "Log out"
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
+              ),
+            ),
+            onPressed: () async{
+              await _auth.signOut();
+
+            },
+
+          ),
+
+        ],
+
       ),
-    );
+      body: Container(
+        child: Text("welcome"),
+      ),
+      );
   }
 }
