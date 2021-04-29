97bf8b8b04c2676caafd0adad62d57c9cdb80d85
Charles Max Crandall <robloxatme.com@gmail.com>
2021-04-29 11:09:20 -0400

    Added new profile.dart in models directory to read from users table to display. Updated profile page to display the rest of the attributes

---
+++ lib/models/profile.dart
@@ -0,0 +1,241 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+//Used to get first name and last name
+class GetUserName extends StatelessWidget {
+  final String documentId;
+
+  GetUserName(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("${data['firstName']} ${data['lastName']}", style: TextStyle(
+              color: Colors.black, fontSize: 30));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get relationship status
+class GetRelationship extends StatelessWidget {
+  final String documentId;
+
+  GetRelationship(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Relationship: ${data['relationship']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get phone
+class GetPhone extends StatelessWidget {
+  final String documentId;
+
+  GetPhone(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Phone: ${data['phone']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get date of birth
+class GetDOB extends StatelessWidget {
+  final String documentId;
+
+  GetDOB(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Date of Birth: ${data['dob']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get Job
+class GetJob extends StatelessWidget {
+  final String documentId;
+
+  GetJob(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Job: ${data['job']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get Gender
+class GetGender extends StatelessWidget {
+  final String documentId;
+
+  GetGender(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Gender: ${data['gender']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+//Used to get Location
+class GetLocation extends StatelessWidget {
+  final String documentId;
+
+  GetLocation(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Location: ${data['location']}", style: TextStyle(color: Colors.black));
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mediacenterflutter/models/profile.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -8,68 +9,6 @@ class ProfilePage extends StatefulWidget {
     return _ProfileState();
   }
 }
-
-// Create a reference to the users collection
-CollectionReference userRef = FirebaseFirestore.instance.collection("users");
-
-// Create a query against the collection.
-Query query = userRef.where("id" == FirebaseAuth.instance.currentUser.uid);
-
-class GetUserName extends StatelessWidget {
-  final String documentId;
-
-  GetUserName(this.documentId);
-
-  @override
-  Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-
-    return FutureBuilder<DocumentSnapshot>(
-      future: users.doc(documentId).get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
-
-        if (snapshot.hasData && !snapshot.data.exists) {
-          return Text("Document does not exist");
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data.data();
-          return Text("${data['firstName']} ${data['lastName']}", style: TextStyle(
-              color: Colors.black, fontSize: 30));
-        }
-
-        return Text("loading");
-      },
-    );
-  }
-}
-
-/*
-DocumentReference docRef = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser.uid);
-docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-  @Override
-  public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-  if (task.isSuccessful()) {
-  DocumentSnapshot document = task.getResult();
-  if (document.exists()) {
-  Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-  } else {
-  Log.d(TAG, "No such document");
-  }
-  } else {
-  Log.d(TAG, "get failed with ", task.getException());
-  }
-  }
-});
- */
-
-
-
 
 class _ProfileState extends State<ProfilePage> {
   @override
@@ -112,8 +51,8 @@ class _ProfileState extends State<ProfilePage> {
                       Container(
                           alignment: Alignment.center,
                           decoration: BoxDecoration(),
-                          child: GetUserName(FirebaseAuth.instance.currentUser.uid)
-                              ),
+                          child: GetUserName(
+                              FirebaseAuth.instance.currentUser.uid)),
                       Container(
                           child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -126,8 +65,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text("Relationship:",
-                                    style: TextStyle(color: Colors.black))),
+                                child: GetRelationship(
+                                    FirebaseAuth.instance.currentUser.uid)),
                             Container(
                                 alignment: Alignment.center,
                                 width: 300,
@@ -136,8 +75,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text("Phone:",
-                                    style: TextStyle(color: Colors.black))),
+                                child: GetPhone(
+                                    FirebaseAuth.instance.currentUser.uid)),
                             Container(
                                 alignment: Alignment.center,
                                 width: 300,
@@ -146,8 +85,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text("Date of Birth:",
-                                    style: TextStyle(color: Colors.black))),
+                                child: GetDOB(
+                                    FirebaseAuth.instance.currentUser.uid)),
                             Container(
                                 alignment: Alignment.center,
                                 width: 300,
@@ -156,8 +95,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text("Job:",
-                                    style: TextStyle(color: Colors.black))),
+                                child: GetJob(
+                                    FirebaseAuth.instance.currentUser.uid)),
                             Container(
                                 alignment: Alignment.center,
                                 width: 300,
@@ -166,10 +105,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text(
-                                  "Gender:",
-                                  style: TextStyle(color: Colors.black),
-                                )),
+                                child: GetGender(
+                                    FirebaseAuth.instance.currentUser.uid)),
                             Container(
                                 alignment: Alignment.center,
                                 width: 300,
@@ -178,8 +115,8 @@ class _ProfileState extends State<ProfilePage> {
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(20),
                                     border: Border.all()),
-                                child: Text("Location:",
-                                    style: TextStyle(color: Colors.black))),
+                                child: GetLocation(
+                                    FirebaseAuth.instance.currentUser.uid)),
                           ])),
                     ]))));
   }
