3e8b980b5232856bea15500607354ef55b876c37
Charles Max Crandall <robloxatme.com@gmail.com>
2021-04-30 09:28:02 -0400

    Users can now edit their own attributes of their profile

--- lib/profile/editprofile.dart
+++ lib/profile/editprofile.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mediacenterflutter/models/profile.dart';
+import 'package:mediacenterflutter/profile/profile.dart';
 
 class EditProfilePage extends StatefulWidget {
   @override
@@ -27,6 +28,26 @@ class _EditProfileState extends State<EditProfilePage> {
               leading: Icon(Icons.arrow_back),
               title: Text('Edit Profile'),
               backgroundColor: Colors.orange,
+            ),
+            floatingActionButton: FloatingActionButton(
+              elevation: 10.0,
+              child: Icon(Icons.edit),
+              onPressed: () {
+                CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+                Future<void> updateUser() {
+                  return users
+                      .doc(FirebaseAuth.instance.currentUser.uid)
+                      .update({'relationship': relationship, 'phone': phone, 'dob': dob, 'job': job, 'gender': gender, 'location': location})
+                      .then((value) => print("User Updated"))
+                      .catchError((error) => print("Failed to update user: $error"));
+                }
+
+                updateUser();
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
             ),
             body: Container(
                 decoration: BoxDecoration(color: Colors.orange),
